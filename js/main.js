16fae31bb3937dfbca098f5c5f0bd99f3305884a
dim-glitch <qwerty9009zxc@gmail.com>
2020-11-11 18:04:24 +0300

    login

--- js/main.js
+++ js/main.js
@@ -9,3 +9,53 @@ menuToggle.addEventListener('click', function (event) {
   // вешаем класс на меню, когда кликнули по кнопке меню 
   menu.classList.toggle('visible');
 })
+
+const loginElem = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('.login-email');
+const passwordInput = document.querySelector('.login-password');
+const loginSignup = document.querySelector('.login-signup');
+
+const listUsers = [{
+    id: '01',
+    email: 'razik_d@mail.ru',
+    password: '12345',
+    displayName: 'dimich'
+  },
+  {
+    id: '02',
+    email: 'hgs@mail.ru',
+    password: '0987',
+    displayName: 'dimich2'
+  }
+];
+
+const setUsers = {
+  user: null,
+  logIn(email, password) {
+
+  },
+  logOut() {
+    console.log('выход');
+  },
+  signUp(email, password) {
+    console.log('регистрация');
+  }
+};
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  setUsers.logIn(emailValue, passwordValue);
+});
+
+loginSignup.addEventListener('click', event => {
+  event.preventDefault();
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  setUsers.signUp(emailValue, passwordValue);
+  setUsers.signUp();
+})
